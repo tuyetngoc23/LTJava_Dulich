273415ad6ef2d14780f0bd271e0c24a4ea5fdac2
tuyetngoc23 <1851050096ngoc@ou.edu.vn>
2021-04-25 16:19:17 +0700

    merge

--- TravelYenNgoc/src/main/java/com/yn/controller/HomeController.java
+++ TravelYenNgoc/src/main/java/com/yn/controller/HomeController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -23,41 +23,19 @@ public class HomeController {
     public String index(Model model){
         return "index";
     }
-    
-    
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.yn.controller;
-
-import com.yn.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-/**
- *
- * @author Huynh Thi Tuyet Ngoc
- */
-@Controller
-public class HomeController {
-    @Autowired
-    private UserService userService;
-    
-    @RequestMapping("/")
-    public String index(Model model){
-       model.addAttribute("user", this.userService.getUser());
-        return "index";
-    }
-    @RequestMapping("/admin")
+     @RequestMapping("/admin")
     public String adindex(Model model){
        //model.addAttribute("user", this.userService.getUser());
         return "admin";
     }
+    
 }
->>>>>>> master
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    
+
